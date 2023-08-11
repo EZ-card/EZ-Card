21688959513476c97264b808c1042cbb9de8b381
김민지 <ghi512151@gmail.com>
2023-08-11 13:57:40 +0900

    feat: 초기 세팅

---
+++ ezcard/src/main/java/com/taba3/team5/ezcard/EzcardApplication.java
@@ -0,0 +1,13 @@
+package com.taba3.team5.ezcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzcardApplication.class, args);
+	}
+
+}
---
+++ ezcard/src/test/java/com/taba3/team5/ezcard/EzcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taba3.team5.ezcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
