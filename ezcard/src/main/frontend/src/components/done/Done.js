60316ece4a8182cced3cc9c3a1755ce6bceffd8a
김민지 <ghi512151@gmail.com>
2023-08-23 18:16:26 +0900

    style: 회원가입 완료 페이지 링크 추가

--- ezcard/src/main/frontend/src/components/done/Done.js
+++ ezcard/src/main/frontend/src/components/done/Done.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Done.css';
 import done from '../../assets/icon/done.png'
+import {Link} from "react-router-dom";
 
 //회원가입 완료 페이지
 const Done = () => (
@@ -10,7 +11,7 @@ const Done = () => (
             <div className="done_box">
                 <div className="done_img"><img src={done}/></div>
                 <p id="text"> 완료되었습니다! </p>
-                <div className="done_button"><button id="doneButton">로그인하고 맞춤 추천 받기</button></div>
+                <div className="done_button"><button id="doneButton"><Link to="/login" className="done_link">로그인하고 맞춤 추천 받기</Link></button></div>
             </div>
         </section>
 
